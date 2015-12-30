ff1e7ecc1a43bc4b8a09e4e31f4e690935bf8f0c
Qianfeng <qianfeng@Illusion.local>
2015-12-30 17:50:46 +0800

    first

---
+++ ZZURecord/YHClass.m
@@ -0,0 +1,13 @@
+//
+//  YHClass.m
+//  ZZURecord
+//
+//  Created by Qianfeng on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import "YHClass.h"
+
+@implementation YHClass
+
+@end
