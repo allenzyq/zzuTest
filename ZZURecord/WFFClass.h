490d7e133f335769a56424c95db04019b37d6088
qianfeng <qianfeng@wangfeng.local>
2015-12-30 19:23:51 +0800

    frist

---
+++ ZZURecord/WFFClass.h
@@ -0,0 +1,13 @@
+//
+//  WFFClass.h
+//  ZZURecord
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WFFClass : NSObject
+
+@end
