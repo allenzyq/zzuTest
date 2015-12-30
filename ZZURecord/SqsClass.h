602af7e1d0ab532b0cc6b9a4ceb9fee909a2ee18
sqs <sqs@sqs.local>
2015-12-30 17:51:41 +0800

    添加姓名

---
+++ ZZURecord/SqsClass.h
@@ -0,0 +1,13 @@
+//
+//  SqsClass.h
+//  ZZURecord
+//
+//  Created by sqs on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SqsClass : NSObject
+
+@end
