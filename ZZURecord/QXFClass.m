90cb96c9789bfcf3b87b0e5d793b67c7a45a797b
QXFqianfeng <QXFqianfeng@QXF.local>
2015-12-30 19:18:21 +0800

    修改了

---
+++ ZZURecord/QXFClass.m
@@ -0,0 +1,13 @@
+//
+//  QXFClass.m
+//  ZZURecord
+//
+//  Created by QXFqianfeng on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import "QXFClass.h"
+
+@implementation QXFClass
+
+@end
