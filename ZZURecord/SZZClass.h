c5508f132cf9e06ab1ee57d8dbc615e3479d5583
qianfeng <qianfeng@Szz.local>
2015-12-30 19:45:49 +0800

    n

---
+++ ZZURecord/SZZClass.h
@@ -0,0 +1,13 @@
+//
+//  SZZClass.h
+//  ZZURecord
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SZZClass : NSObject
+
+@end
