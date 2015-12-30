fc6b0160b828e098ad12d1ec8c6bcbc9861c8b53
JackWong <me@iluckly.com>
2015-12-30 16:39:13 +0800

    1517 测试 gitdemo

---
+++ ZZURecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZURecord
+//
+//  Created by JackWong on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
