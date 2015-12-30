1f516c07307f9434e698e538c9f8d10350b1309e
qianfeng <qianfeng@LMD.local>
2015-12-30 19:57:05 +0800

    gaoge

---
+++ ZZURecord/GGClass.m
@@ -0,0 +1,13 @@
+//
+//  GGClass.m
+//  ZZURecord
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import "GGClass.h"
+
+@implementation GGClass
+
+@end
