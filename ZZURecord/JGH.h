6f9cd391cdbd11ea66d23c2228806567fa27779e
HuH <qianfeng1@HuH.local>
2015-12-30 17:07:22 +0800

    asfa

---
+++ ZZURecord/JGH.h
@@ -0,0 +1,13 @@
+//
+//  JGH.h
+//  ZZURecord
+//
+//  Created by HuH on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGH : NSObject
+
+@end
---
+++ ZZURecord/JGH.m
@@ -0,0 +1,13 @@
+//
+//  JGH.m
+//  ZZURecord
+//
+//  Created by HuH on 15/12/30.
+//  Copyright © 2015年 JackWong. All rights reserved.
+//
+
+#import "JGH.h"
+
+@implementation JGH
+
+@end
